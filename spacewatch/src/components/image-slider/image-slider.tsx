25778604c5135f8cb645baf4d5d73fd86a1f3212
Sourav <55662036+souravkashyap0@users.noreply.github.com>
2025-05-19 23:07:46 +0530

    Image Slider Added

--- spacewatch/src/components/image-slider/image-slider.tsx
+++ spacewatch/src/components/image-slider/image-slider.tsx
@@ -1,19 +1,65 @@
+import { Box, Button , styled } from '@mui/material';
 import {useState , useEffect} from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 interface ImageSliderProps {
     roverDetails : any;
 }
 
+const StyledImage = styled('img')`
+objectFit :cover;
+width : 100%;
+height :100%;
+display : block;
+`
+
+const  StyledButton = styled(Button)`
+all : unset;
+display :block;
+position : absolute;
+top: 0;
+bottom : 0;
+padding : 1rem;
+cursor :  pointer;
+color : white;
+`
+
 export default function ImageSlider (marsRoverImageDetails : ImageSliderProps){
     const [currentIndex , setCurrentIndex] = useState(0);
+    const currentImage = marsRoverImageDetails?.roverDetails[currentIndex];
+    
+    function showPrevousImage(){
+      setCurrentIndex((prev)=>{
+        if(prev === 0){
+          return marsRoverImageDetails.roverDetails.length -1;
+        }
+        return prev-1;
+      })
+    }
 
+    function showNextImage () {
+      setCurrentIndex((next)=>{
+        if(next === marsRoverImageDetails.roverDetails.length -1){
+          return 0;
+        }
+        return next+1;
+      })
+    }
     return (
-       <div>
-         <h1>ImageSlider</h1>
-         <img src={marsRoverImageDetails?.roverDetails?.[0]?.img_src} alt='image' />
+      <>
+        <div style={{width :'100%',height : '100%',position :'relative'}}>
+         {currentImage && (<StyledImage src={currentImage.img_src} alt='image' />)}
+         <StyledButton onClick={showPrevousImage} style={{left:0}}><FontAwesomeIcon icon={faArrowLeft} /></StyledButton>
+         <StyledButton onClick={showNextImage} style={{right:0}}><FontAwesomeIcon icon={faArrowRight} /></StyledButton>
        </div>
+      <Box>
+        <p>ROVER NAME : {currentImage?.rover_details.name}</p>
+        <p>EARTH DATE : {currentImage?.earth_date}</p>
+        <p>CAMERA NAME : {currentImage?.camera?.full_name}</p>
+        
+      </Box>
+      </>
        
     )
-
 }
--- spacewatch/src/components/mars-rover/mars-rover.tsx
+++ spacewatch/src/components/mars-rover/mars-rover.tsx
@@ -1,8 +1,13 @@
 import {useState,useEffect} from 'react';
 import  ImageSlider from '../image-slider/image-slider.tsx';
+import DatePicker from '@mui/lab/DatePicker';
+import {TextField} from '@mui/material';
+
 
 export  default  function MarsRover () {
     const [roverData , setRoverData] = useState<any>();
+    const [dateValue , setDateValue] = useState<Date | null>(null);
+
     useEffect(()=>{
         const fetchRoverDetails =  async()=>{
             const response =  await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2025-4-3&api_key=${import.meta.env.VITE_NASA_API_KEY }`);
@@ -28,10 +33,21 @@ export  default  function MarsRover () {
         return  marsRoverImageDetails;
     }
     const  marsRoverImageDetails = generateMarsRoverImageDetails(roverData);
+    console.log('THE DATE ',dateValue);
     return  (
-            <div>
-                <h1>MARS ROVER</h1>
+        <>
+        {/* <DatePicker
+            label="Basic example"
+            value={dateValue}
+            onChange={(newValue: any) => {
+                setDateValue(newValue);
+            }}
+            renderInput={(params :any) => <TextField {...params} />}/> */}
+
+            <h1 style={{textAlign :'center'}}>Mars Rover Image</h1>
+             <div style={{maxWidth :'1200px',width:'100%',height:'500px',margin: '0px auto'}}>
                 {marsRoverImageDetails && <ImageSlider roverDetails={marsRoverImageDetails} />}
             </div>
+        </>    
     )
 }
